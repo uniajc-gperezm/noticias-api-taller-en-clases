62a65d23d5a5cfc58f83b20d1d443b2a45ccd34c
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-10-30 20:40:23 -0500

    creando servidor express con ruta inicial y ruta para noticias

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hola Mundo desde API Noticias Taller en Clases");
+const express = require('express'); // Importa el framework Express
+const app = express(); // Crea una aplicación Express
+const port = 3000; // Puedes usar cualquier puerto disponible
+
+// Define una ruta básica inicial para probar el servidor
+app.get('/', (req, res) => {
+  res.send('¡Hola Mundo desde mi primera API con Express!');
+});
+
+app.get('/noticias', (req, res) => {
+  
+    const noticias = [
+      { id: 1, titulo: 'Noticia 1', contenido: 'Contenido de la noticia 1' },
+      { id: 2, titulo: 'Noticia 2', contenido: 'Contenido de la noticia 2' },
+      { id: 3, titulo: 'Noticia 3', contenido: 'Contenido de la noticia 3' },
+    ];
+
+    res.json(noticias);
+});
+
+// Inicia el servidor y escucha en el puerto definido
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
